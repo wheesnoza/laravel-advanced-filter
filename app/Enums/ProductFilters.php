dbb3fb87669a4fa6c3671c5be73bcb4d2ac0c000
wheesnoza <wheesnoza@gmail.com>
2023-04-04 16:37:30 +0900

    Change filters to variants.

--- app/Enums/ProductFilters.php
+++ app/Enums/ProductFilters.php
@@ -5,17 +5,18 @@ namespace App\Enums;
 use App\Filter\Filter;
 use App\Filter\ProductNameFilter;
 use App\Filter\ProductPriceFilter;
+use App\Filter\ProductProviderFilter;
 
 enum ProductFilters: string
 {
-    case Name = 'name';
     case Price = 'price';
+    case Provider = 'provider';
 
     public function createFilter(mixed $value): Filter
     {
         return match ($this) {
-            self::Name => new ProductNameFilter($value),
             self::Price => new ProductPriceFilter($value),
+            self::Provider => new ProductProviderFilter($value),
         };
     }
 }
