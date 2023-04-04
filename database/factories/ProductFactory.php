22d61019afcf0612d91d0a9320ad3f55ac969946
wheesnoza <wheesnoza@gmail.com>
2023-04-05 08:44:11 +0900

    Change product items to clothes brands.

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -19,7 +19,7 @@ class ProductFactory extends Factory
         return [
             'name' => fake()->name(),
             'description' => fake()->text(),
-            'brand' => fake()->randomElement(['ASUS', 'Apple', 'Microsoft', 'IKEA']),
+            'brand' => fake()->randomElement(['GU', 'H&M', 'UNIQLO', '無印良品']),
         ];
     }
 }
