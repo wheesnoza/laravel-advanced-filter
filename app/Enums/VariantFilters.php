c79eaf370ecba480cc26f75f334f87327c0cf8d3
wheesnoza <wheesnoza@gmail.com>
2023-04-05 08:41:07 +0900

    Fix filter url query parameter key name.

--- app/Enums/VariantFilters.php
+++ app/Enums/VariantFilters.php
@@ -5,25 +5,28 @@ namespace App\Enums;
 use App\Filters\VariantPriceFilter;
 use App\Filters\VariantPriceGreaterThanEqualFilter;
 use App\Filters\VariantPriceLowerThanEqualFilter;
+use App\Filters\VariantProductBrandFilter;
 use App\Filters\VariantProductNameFilter;
 use App\Filters\VariantProductSizeInFilter;
 use App\Filters\VariantRaitingFilter;
 
 enum VariantFilters: string
 {
-    case PriceGreaterThanEqual = 'gte:price';
-    case PriceLowerThanEqual = 'lte:price';
     case Price = 'price';
-    case SizeIn = 'size:in';
-    case Raiting = 'raiting';
     case Name = 'name';
+    case Raiting = 'raiting';
+    case Brand = 'brand';
+    case PriceGreaterThanEqual = 'price:gte';
+    case PriceLowerThanEqual = 'price:lte';
+    case SizeIn = 'size:in';
 
     public function createFilter($value)
     {
         return match ($this) {
             self::Price => new VariantPriceFilter($value),
-            self::Raiting => new VariantRaitingFilter($value),
             self::Name => new VariantProductNameFilter($value),
+            self::Raiting => new VariantRaitingFilter($value),
+            self::Brand => new VariantProductBrandFilter($value),
             self::PriceGreaterThanEqual => new VariantPriceGreaterThanEqualFilter($value),
             self::PriceLowerThanEqual => new VariantPriceLowerThanEqualFilter($value),
             self::SizeIn => new VariantProductSizeInFilter($value),
