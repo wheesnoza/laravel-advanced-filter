6d0e508ba6c82f566681881dbf757962472f416e
wheesnoza <wheesnoza@gmail.com>
2023-04-05 20:42:44 +0900

    Install and configure Inertia with react and typescript.

---
+++ resources/ts/Pages/Products.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+interface ProductsProps {}
+
+const Products: React.FC<ProductsProps> = (props) => {
+  return <>Welcome from TSX</>;
+};
+export default Products;
--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,16 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        host: true,
+        hmr: {
+            host: 'localhost'
+        },
+        watch: {
+            usePolling: true
+        }
+    },
     plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
+        laravel(["resources/ts/app.tsx"]),
     ],
 });
