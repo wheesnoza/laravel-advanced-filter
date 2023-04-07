855037254d8533139f5b2dc83c5c709542548f75
wheesnoza <wheesnoza@gmail.com>
2023-04-07 22:38:40 +0900

    Append return type.

--- app/Actions/Product/GetTopFivePopularProducts.php
+++ app/Actions/Product/GetTopFivePopularProducts.php
@@ -2,11 +2,12 @@
 
 namespace App\Actions\Product;
 
+use App\Collections\Product\ProductCollection;
 use App\Models\Variant;
 
 class GetTopFivePopularProducts
 {
-    public static function execute()
+    public static function execute(): ProductCollection
     {
         return Variant::with('product')
             ->orderByDesc('raiting')
