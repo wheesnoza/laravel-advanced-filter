94a4d2b4e5506328ec43af6bd106333710254a7a
wheesnoza <45123151+wheesnoza@users.noreply.github.com>
2023-04-16 00:18:43 +0900

    Fix filter collection to array bug.

--- app/Collections/FilterCollection.php
+++ app/Collections/FilterCollection.php
@@ -2,37 +2,21 @@
 
 namespace App\Collections;
 
-use App\Enums\FilterValues;
-use App\Enums\Product\ProductFilters;
-use App\Filters\Filter;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Collection;
+use App\Enums\Product\ProductFilters;
+use App\Enums\FilterValues;
 use Illuminate\Support\Str;
 
 class FilterCollection extends Collection
 {
-    /**
-     * @var Filter[]
-     */
-    protected $items;
-
-    /**
-     * @param array<string, mixed> $items
-     */
-    public function __construct($items = [])
-    {
-        $this->items = collect($items)
-            ->map(function ($value, $name) {
-                $type = Str::after($name, ':');
-                $value = FilterValues::tryFrom($type)?->create($value) ?: $value;
-                return ProductFilters::tryFrom($name)?->create($value);
-            })
-            ->filter()
-            ->all();
-    }
-
     public function handle(Builder $query): void
     {
-        $this->each(fn ($filter) => $filter->handle($query));
+        $this->map(function ($value, $filter) {
+            $value = FilterValues::tryFrom(Str::after($filter, ':'))?->create($value) ?: $value;
+            return ProductFilters::tryFrom($filter)?->create($value);
+        })
+        ->filter()
+        ->each(fn ($filter) => $filter->handle($query));
     }
 }
